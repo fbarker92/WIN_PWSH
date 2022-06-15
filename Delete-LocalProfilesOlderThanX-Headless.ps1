3349b62b7a9cddd68493b2538000e21fe63cdfad
fbarker92 <f.barker92@gmail.comforeach ( in ) {>
2022-06-15 22:17:29 +0100

    commit

--- Delete-LocalProfilesOlderThanX-Headless.ps1
+++ Delete-LocalProfilesOlderThanX-Headless.ps1
@@ -1,5 +1,7 @@
+# Set local profiles to be protected from deletion
 $ExcludedProfiles = "Default", "Public", "fbark", "ldadmin"
 
+# Load local profiles from Users folder
 $LocalProfiles = Get-ChildItem -path C:\Users\ -Exclude $ExcludedProfiles | Where-Object lastwritetime -lt (Get-Date).AddDays(0) | Select-Object name
 
     foreach($LocalProfile in $LocalProfiles){
